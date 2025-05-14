bf5617647535f8067d7ee46709dc515bc4b675f3
Whirligig231 <gigilrihw@gmail.com>
2025-05-14 00:15:52 -0400

    An actual audio stream! A shitty ear-melting one but hey!

--- Retempo2/src/audiocore/AudioStream.cs
+++ Retempo2/src/audiocore/AudioStream.cs
@@ -1,4 +1,6 @@
-﻿using PortAudioSharp;
+﻿using System.Runtime.InteropServices;
+using PortAudioSharp;
+using Stream = PortAudioSharp.Stream;
 
 namespace Retempo2
 {
@@ -6,7 +8,32 @@ namespace Retempo2
 	{
 		private static bool initializedPortAudio;
 
-		public AudioStream()
+		private double sampleRate;
+		private int channels;
+		private float bufferLength;
+
+		private float[] circleBuffer;
+		private int bufferIndex; // Index of a single float in the circular buffer
+		private long frameIndex; // Index of the frame in the linear stream
+
+		private Stream stream;
+
+		private StreamCallbackResult CircularBufferCallback(IntPtr input, IntPtr output, UInt32 frameCount,
+			ref StreamCallbackTimeInfo timeInfo, StreamCallbackFlags statusFlags, IntPtr userData)
+		{
+			float[] temp = new float[frameCount * channels];
+			for (int i = 0; i < frameCount; i++)
+			{
+				for (int j = 0; j < channels; j++)
+				{
+					temp[i * channels + j] = MathF.Sin((float)(i * channels) / (float)(sampleRate) * 440.0f * 2.0f * MathF.PI) / 10.0f;
+				}
+			}
+            Marshal.Copy(temp, 0, output, (int)(frameCount * channels));
+			return StreamCallbackResult.Continue;
+		}
+
+		public AudioStream(float bufferLength = 1.0f)
 		{
 			if (!initializedPortAudio)
 			{
@@ -21,7 +48,20 @@ namespace Retempo2
 			}
 
 			DeviceInfo deviceInfo = PortAudio.GetDeviceInfo(deviceIndex);
-			MessageBox.Show(deviceInfo.ToString());
+			sampleRate = deviceInfo.defaultSampleRate;
+			channels = deviceInfo.maxOutputChannels;
+
+			StreamParameters parameters = new StreamParameters();
+			parameters.device = deviceIndex;
+			parameters.channelCount = channels;
+			parameters.sampleFormat = SampleFormat.Float32;
+			parameters.suggestedLatency = deviceInfo.defaultLowOutputLatency;
+
+			Stream.Callback callback = CircularBufferCallback;
+
+			stream = new Stream(inParams: null, outParams: parameters, sampleRate: sampleRate,
+				framesPerBuffer: 0, streamFlags: StreamFlags.NoFlag, callback: callback, userData: IntPtr.Zero);
+			stream.Start();
 		}
 	}
 }
