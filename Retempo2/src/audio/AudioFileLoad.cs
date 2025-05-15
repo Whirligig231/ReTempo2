cb5ef87f823dc39e6f44fe49de38fa4dfcdafc85
Whirligig231 <gigilrihw@gmail.com>
2025-05-15 03:28:31 -0400

    .ogg support

--- Retempo2/src/audio/AudioFileLoad.cs
+++ Retempo2/src/audio/AudioFileLoad.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using NAudio.Vorbis;
 using NAudio.Wave;
 
 namespace Retempo2
@@ -9,17 +10,43 @@ namespace Retempo2
         {
             if (!File.Exists(fname))
                 return null;
-            MediaFoundationReader reader;
-            try
+
+            WaveStream reader;
+
+            bool isOgg = false;
+            if (Path.GetExtension(fname) == ".ogg" || Path.GetExtension(fname) == ".OGG")
             {
-                reader = new MediaFoundationReader(fname);
-            } catch (COMException e)
+                isOgg = true;
+                try
+                {
+                    reader = new VorbisWaveReader(fname);
+                }
+                catch (COMException e)
+                {
+                    MessageBox.Show("This audio file is invalid.", null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+            else
             {
-                MessageBox.Show("This audio file is invalid.", null, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return null;
+                try
+                {
+                    reader = new MediaFoundationReader(fname);
+                }
+                catch (COMException e)
+                {
+                    MessageBox.Show("This audio file is invalid.", null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
             }
             ISampleProvider isp = reader.ToSampleProvider();
-            float[] buffer = new float[reader.Length / 2];
+            long lengthToRead = reader.Length / 2;
+            // Unknown why OGG does this
+            if (isOgg)
+            {
+                lengthToRead /= 2;
+            }
+            float[] buffer = new float[lengthToRead];
             isp.Read(buffer, 0, buffer.Length);
             return buffer;
         }
--- Retempo2/src/utility/DialogSupport.cs
+++ Retempo2/src/utility/DialogSupport.cs
@@ -5,7 +5,7 @@ namespace Retempo2
         public static string? GetAudioFname()
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Audio files(*.aac;*.mp3;*.m4a;*.wav)|*.AAC;*.MP3;*.M4A;*.WAV|All files (*.*)|*.*";
+            ofd.Filter = "Audio files(*.aac;*.mp3;*.m4a;*.wav;*.ogg)|*.AAC;*.MP3;*.M4A;*.WAV;*.OGG|All files (*.*)|*.*";
             ofd.RestoreDirectory = true;
             DialogResult res = ofd.ShowDialog();
             if (res == DialogResult.OK)
