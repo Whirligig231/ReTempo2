230c5c5b11cc435efce21765c1a13051f5aab71b
Whirligig231 <gigilrihw@gmail.com>
2025-05-18 18:33:29 -0400

    Float list to string, for clipboard actions

---
+++ Retempo2/src/utility/FloatListString.cs
@@ -0,0 +1,30 @@
+﻿namespace Retempo2
+{
+    public static class FloatListString
+    {
+        public static string FloatsToString(IEnumerable<float> values)
+        {
+            string output = "";
+            foreach (float value in values)
+            {
+                if (output != "")
+                    output += " ";
+                output += value.ToString();
+            }
+            return output;
+        }
+
+        public static float[] StringToFloats(string str)
+        {
+            List<float> output = new List<float>();
+            string[] words = str.Split(' ');
+            foreach (string word in words)
+            {
+                float val;
+                if (float.TryParse(str, out val))
+                    output.Add(val);
+            }
+            return output.ToArray();
+        }
+    }
+}
